5d8d4c4beda3a3c71c72c5e0ef3fe37c62ec65ce
andrebaran <andrebarandossantos@yahoo.com.br>
2020-09-11 13:13:50 -0300

    commit fk quarto reserva

--- app/Quarto.php
+++ app/Quarto.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Quarto extends Model
 {
     //
+    protected $fillable = [
+        'hotel_id','capacidade','valor','disponivel'       
+    ];
+    public function hotel()
+    {
+      return $this->hasOne('App\Hotel', 'hotel_id');
+    }
+
 }
--- app/Reserva.php
+++ app/Reserva.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Reserva extends Model
 {
     //
+
+    protected $fillable = [
+        'localidade','pessoa_id','quarto_id','data_reserva'       
+    ];
+    public function pessoa()
+    {
+      return $this->hasOne('App\Pessoa', 'pessoa_id');
+    }
+    public function quarto()
+    {
+      return $this->hasOne('App\Quarto', 'quarto_id');
+    }
 }
